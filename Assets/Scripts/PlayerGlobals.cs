5071cde02e3fe84391192d250aa73de7a4195c95
Dan Banfield <dandbanfield@gmail.com>
2022-06-07 19:54:15 +0100

    Updated the player globals script.
    Added the 'GetPlayerName' method which returns the cached player name fetched from the 'PlayerPrefs' class.

--- Assets/Scripts/PlayerGlobals.cs
+++ Assets/Scripts/PlayerGlobals.cs
@@ -2,16 +2,25 @@ using UnityEngine;
 
 public static class PlayerGlobals
 {
-    public static string PlayerName
+    private static string PlayerName
     {
         get
         {
             return PlayerPrefs.GetString("name");
         }
-        private set
+        set
         {
             PlayerPrefs.SetString("name", value);
         }
+    }
+
+    private static string cachedPlayerName;
+
+    public static string GetPlayerName()
+    {
+        if (string.IsNullOrEmpty(cachedPlayerName))
+            cachedPlayerName = PlayerName;
+        return cachedPlayerName;
     }
 
     public static void SetPlayerName(string name)
