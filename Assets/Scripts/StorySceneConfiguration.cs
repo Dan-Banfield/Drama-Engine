61c325b37c8d2046a07a75c90e423401c02fcfde
Dan Banfield <dandbanfield@gmail.com>
2022-06-23 18:52:35 +0100

    Updated the story scene.
    Created a base script to initialize the story. E.g. Jump to the current position in the story.

---
+++ Assets/Scripts/StorySceneConfiguration.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class StorySceneConfiguration : MonoBehaviour
+{
+    private void Awake()
+    {
+        // TODO: Initialize the story. E.g. Jump to the current chapter and dialogue line.
+    }
+}
